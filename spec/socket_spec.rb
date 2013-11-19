8d754c30180330b0b21ed045a46d7f082ab3e7ba
Alex McLain <alex@alexmclain.com>
2013-11-18 19:44:53 -0800

    Created socket spec.

---
+++ spec/socket_spec.rb
@@ -0,0 +1,22 @@
+require 'ionian/socket'
+
+describe Ionian::Socket do
+  
+  # Server needs to be a separate class.
+  it "can be instantitated as a TCP, UDP, Unix server socket"
+  
+  it "can be instantiated as a TCP, UDP, Unix client socket"
+  
+  it "extends the Ionian IO extentions"
+  
+  it "extends the Ionian socket extentions"
+  
+  it "can open a TCP client as non-persistent (closes after message received)"
+  
+  it "can open a Unix client as non-persistent (closes after message received)"
+  
+  it "can open a TCP client as send-and-forget (closes after TX)"
+  
+  it "can open a Unix client as send-and-forget (closes after TX)"
+  
+end
