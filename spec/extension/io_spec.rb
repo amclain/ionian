e7dc2917f11adadaa8844de0911fe36a3346f255
Alex McLain <alex@alexmclain.com>
2013-11-18 19:12:41 -0800

    Removed parenthesis from spec matches.

--- spec/extension/io_spec.rb
+++ spec/extension/io_spec.rb
@@ -37,19 +37,19 @@ describe Ionian::Extension::IO do
   it "can get and set the IO timeout" do
     value = 5
     @ionian.ionian_timeout = value
-    @ionian.ionian_timeout.should eq(value)
+    @ionian.ionian_timeout.should eq value
   end
   
   it "can get and set the regex match expression" do
     value = /test/
     @ionian.expression = value
-    @ionian.expression.should eq(value)
+    @ionian.expression.should eq value
   end
   
   it "can purge Ionian's read buffer" do
     @client.puts 'test data'
     @ionian.purge
-    @ionian.read_match(timeout: 0).should eq(nil)
+    @ionian.read_match(timeout: 0).should eq nil
   end
   
   it "can read matched data" do
@@ -58,21 +58,21 @@ describe Ionian::Extension::IO do
     
     match = @ionian.read_match
     
-    match[0].cmd.should   eq('CS')
-    match[0].param.should eq('1234')
-    match[0].value.should eq('1')
-    match[1].cmd.should   eq('CS')
-    match[1].param.should eq('4567')
-    match[1].value.should eq('0')
+    match[0].cmd.should   eq 'CS'
+    match[0].param.should eq '1234'
+    match[0].value.should eq '1'
+    match[1].cmd.should   eq 'CS'
+    match[1].param.should eq '4567'
+    match[1].value.should eq '0'
   end
   
   it "can receive matched data on a listener" do
     block_run = false
     
     @ionian.on_match do |match, socket|
-      match.cmd.should   eq('CS')
-      match.param.should eq('7890')
-      match.value.should eq('1')
+      match.cmd.should   eq 'CS'
+      match.param.should eq '7890'
+      match.value.should eq '1'
       
       block_run = true
     end
@@ -86,7 +86,7 @@ describe Ionian::Extension::IO do
     thread.kill
     
     Timeout.timeout 1 do; thread.join; end
-    block_run.should eq(true)
+    block_run.should eq true
   end
   
   it "calls a listener block once for each match" do
@@ -96,13 +96,13 @@ describe Ionian::Extension::IO do
     @ionian.on_match do |match, socket|
       case match_count
       when 0
-        match.cmd.should   eq('CS')
-        match.param.should eq('7890')
-        match.value.should eq('1')
+        match.cmd.should   eq 'CS'
+        match.param.should eq '7890'
+        match.value.should eq '1'
       when 1
-        match.cmd.should   eq('CS')
-        match.param.should eq('2345')
-        match.value.should eq('0')
+        match.cmd.should   eq 'CS'
+        match.param.should eq '2345'
+        match.value.should eq '0'
         
         block_run = true
       end
@@ -119,7 +119,7 @@ describe Ionian::Extension::IO do
     thread.kill
     
     Timeout.timeout 1 do; thread.join; end
-    block_run.should eq(true)
+    block_run.should eq true
   end
   
   it "responds to register_observer" do
