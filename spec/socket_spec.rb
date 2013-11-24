63614f88f22cb06e191e1d0b58a346836cd2ac9e
Alex McLain <alex@alexmclain.com>
2013-11-24 14:37:54 -0800

    Revised Ionian::Socket tests.

--- spec/socket_spec.rb
+++ spec/socket_spec.rb
@@ -11,14 +11,32 @@ describe Ionian::Socket do
   include_examples "socket extension interface"
   
   
-  it "can be instantiated as a TCP, UDP, Unix client socket"
+  it "can be instantiated as a TCP client socket"
   
-  it "can open a TCP client as non-persistent (closes after message received)"
+  it "can be instantiated as a UDP client socket"
   
-  it "can open a Unix client as non-persistent (closes after message received)"
+  it "can be instantiated as a Unix client socket"
   
-  it "can open a TCP client as send-and-forget (closes after TX)"
+  it "defaults to TCP if the socket type is not specified"
   
-  it "can open a Unix client as send-and-forget (closes after TX)"
+  it "defaults to persistent"
+  
+  it "can open a persistent TCP client (standard: stays open)"
+  
+  it "can open a persistent UDP client (standard: stays open)"
+  
+  it "can open a persistent Unix client (standard: stays open)"
+  
+  it "can open a non-persistent TCP client (closes after message received)"
+  
+  it "can open a non-persistent Unix client (closes after message received)"
+  
+  it "igores the non-persistent flag for UDP sockets"
+  
+  it "can open a send-and-forget TCP client (closes after TX)"
+  
+  it "can open a send-and-forget Unix client (closes after TX)"
+  
+  it "ignores the send-and-forget flag for UDP sockets"
   
 end
