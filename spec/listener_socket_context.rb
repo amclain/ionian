fbfceb3f98e5f936a5406d9327d54a63582bafa8
Joe McIlvain <joe.eli.mac@gmail.com>
2013-12-03 00:11:58 -0500

    Further refactoring of shared examples

--- spec/listener_socket_context.rb
+++ spec/listener_socket_context.rb
@@ -2,15 +2,81 @@ require 'ionian/extension/io'
 require 'socket'
 require 'timeout'
 
+# Raise exceptions out of child threads to interrupt main thread
+Thread.abort_on_exception = true
 
-def wait_until(timeout_time=1)
-  Timeout.timeout(timeout_time) { Thread.pass until yield }
+# Convenience method for waiting until a condition is true, with a timeout
+def wait_until(timeout:1)
+  Timeout.timeout(timeout) { Thread.pass until Thread.exclusive { yield } }
+end
+
+
+shared_context "listener socket" do
+  
+  before do
+    @server = server
+    @server_thread = Thread.new do
+      loop do
+        begin
+          break if @server.closed?
+          new_request = ::IO.select [@server], nil, nil
+          
+          Thread.exclusive do
+            if new_request
+              @client.close if @client and not @client.closed?
+              @client = @server.accept.extend Ionian::Extension::Socket
+            end
+          end
+        rescue Exception
+          break
+        end
+      end
+    end
+  end
+  
+  after do
+    @client.close if @client and not @client.closed?
+    @server.close if @server and not @server.closed?
+    @server_thread.kill if @server_thread
+    Timeout.timeout(1) { @server_thread.join } if @server_thread
+    
+    @client = nil
+    @server = nil
+    @server_thread = nil
+  end
+  
+end
+
+
+shared_context "tcp listener socket" do
+  let(:port)   { 5050 }
+  let(:server) { TCPServer.new port }
+  
+  include_context "listener socket"
+end
+
+
+shared_context "unix listener socket" do
+  let(:socket_file) { '/tmp/ionian.test.sock' }
+  let(:server)      { 
+    File.delete socket_file if File.exists? socket_file
+    UNIXServer.new socket_file
+  }
+  after { File.delete socket_file if File.exists? socket_file }
+  
+  include_context "listener socket"
+end
+
+
+shared_context "udp listener socket" do |extension|
+  let(:port) { 5050 }
+  # let(:server) {...}
+  # include_context "listener socket"
 end
 
 
 shared_context "ionian subject" do |extension|
-  include_context "listener socket"
-  
+  include_context "tcp listener socket"
   subject { @ionian }
   
   before do
@@ -24,92 +90,9 @@ shared_context "ionian subject" do |extension|
     # This prevents the tests from running until the client is created
     wait_until { @client }
   end
-end
-
-
-shared_context "listener socket" do |extension|
-  
-  let(:port) { 5050 }
-  
-  before do
-    @server = TCPServer.new port
-    
-    @server_thread = Thread.new do
-      loop do
-        begin
-          break if @server.closed?
-          new_request = ::IO.select [@server], nil, nil
-          
-          if new_request
-            @client.close if @client and not @client.closed?
-            @client = @server.accept.extend Ionian::Extension::Socket
-          end
-        rescue Exception
-          break
-        end
-      end
-    end
-    
-  end
   
   after do
     @ionian.close if @ionian and not @ionian.closed?
-    @client.close if @client and not @client.closed?
-    @server.close if @server and not @server.closed?
-    @server_thread.kill if @server_thread
-    wait_until { @server_thread.join } if @server_thread
-    
-    @server = nil
-    @client = nil
-    @ionian = @object = nil
-    @server_thread = nil
+    @ionian = nil
   end
-  
-end
-
-
-shared_context "unix listener socket" do
-  
-  let(:socket_file) { '/tmp/ionian.test.sock' }
-  
-  before do
-    # Unix socket test server.
-    File.delete socket_file if File.exists? socket_file
-    @server = UNIXServer.new socket_file
-    
-    @server_thread = Thread.new do
-      loop do
-        begin
-          break if @server.closed?
-          new_request = ::IO.select [@server], nil, nil
-          
-          if new_request
-            @client.close if @client and not @client.closed?
-            @client = @server.accept.extend Ionian::Extension::Socket
-          end
-        rescue Exception
-          break
-        end
-      end
-    end
-    
-    # wait_until { @client }
-  end
-  
-  after do
-    @server.close if @server and not @server.closed?
-    @server = nil
-    File.delete socket_file if File.exists? socket_file
-    
-    @server_thread.kill if @server_thread
-    Timeout.timeout 1 do; @server_thread.join; end
-  end
-  
-end
-
-
-shared_context "udp listener socket" do |extension|
-  
-  let(:port) { 5050 }
-  
 end
