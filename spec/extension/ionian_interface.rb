4201b50015b08b239092f511a43201865f1035b2
Joe McIlvain <joe.eli.mac@gmail.com>
2013-12-02 23:55:13 -0500

    Refactor test interface shared_examples to take advantage of rspec's automatic test description generation for simple tests

--- spec/extension/ionian_interface.rb
+++ spec/extension/ionian_interface.rb
@@ -1,43 +1,13 @@
+
 shared_examples "ionian interface" do
-  
-  it "responds to initialize_ionian" do
-    subject.should respond_to :initialize_ionian
-  end
-  
-  it "responds to has_data?" do
-    subject.should respond_to :has_data?
-  end
-  
-  it "responds to expression" do
-    subject.should respond_to :expression
-  end
-  
-  it "responds to expression=" do
-    subject.should respond_to :expression=
-  end
-  
-  it "responds to read_match" do
-    subject.should respond_to :read_match
-  end
-  
-  it "responds to run_match" do
-    subject.should respond_to :run_match
-  end
-  
-  it "responds to purge" do
-    subject.should respond_to :purge
-  end
-  
-  it "responds to register_observer" do
-    subject.should respond_to :register_observer
-  end
-  
-  it "responds to on_match" do
-    subject.should respond_to :on_match
-  end
-  
-  it "responds to unregister_observer" do
-    subject.should respond_to :unregister_observer
-  end
-  
+  it { should respond_to :initialize_ionian   }
+  it { should respond_to :has_data?           }
+  it { should respond_to :expression          }
+  it { should respond_to :expression=         }
+  it { should respond_to :read_match          }
+  it { should respond_to :run_match           }
+  it { should respond_to :purge               }
+  it { should respond_to :register_observer   }
+  it { should respond_to :on_match            }
+  it { should respond_to :unregister_observer }
 end
--- spec/extension/socket_extension_interface.rb
+++ spec/extension/socket_extension_interface.rb
@@ -1,11 +1,5 @@
+
 shared_examples "socket extension interface" do
-  
-  it "responds to no_delay" do
-    subject.should respond_to :no_delay
-  end
-  
-  it "responds to no_delay=" do
-    subject.should respond_to :no_delay=
-  end
-  
+  it { should respond_to :no_delay }
+  it { should respond_to :no_delay= }
 end
