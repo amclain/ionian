a39c2102d7316af08230ef88354e6a1d4a56aa8a
Alex McLain <alex@alexmclain.com>
2014-01-19 21:48:39 -0800

    Implemented Server #listen and #close.

--- lib/ionian/server.rb
+++ lib/ionian/server.rb
@@ -1,3 +1,6 @@
+require 'socket'
+require 'ionian/socket'
+
 module Ionian
   
   # A convenient wrapper for TCP, UDP, and Unix server sockets.
@@ -6,16 +9,32 @@ module Ionian
     def initialize **kwargs, &block
       @accept_listeners = []
       register_accept_listener &block if block_given?
+      
+      @interface = kwargs.fetch :interface, ''
+      @port      = kwargs.fetch :port
+      @protocol  = kwargs.fetch :protocol, :tcp
+      
+      # TODO: Needs to account for different protocols.
+      @server   = TCPServer.new @port
     end
     
     # Starts the socket server listening for connections.
     # Blocks registered with #register_accept_listener will
     # be run when a connection is accepted.
     def listen
+      @accept_thread = Thread.new do
+        # Package in an Ionian::Socket
+        client = Ionian::Socket.new @server.accept
+        
+        @accept_listeners.each do |listener|
+          listener.call client
+        end
+      end
     end
     
     # Shutdown the server socket and stop listening for connections.
     def close
+      @server.close if @server
     end
     
     # Register a block to be run when server accepts a client connection.
